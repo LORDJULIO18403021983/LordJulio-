68407a1f43dfbded3e74ef1eb71d7110b76fd945
LORDJULIO18403021983 <103298906+LORDJULIO18403021983@users.noreply.github.com>
2022-06-29 11:29:42 -0300

    Add files via upload

---
+++ whileDeterminado.java
@@ -0,0 +1,13 @@
+package Br.com.aula69.udemy.java;
+
+public class whileDeterminado {
+
+  public static void main(String[] args) {
+  
+     int contador = 0;
+       while ( contador < 20 ) {
+   System.out.printf("i = %d\n ", contador);
+          contador += 2;
+        }
+    }
+}
